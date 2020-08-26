cb32fe92ac5d5b7543a4af4cb0d8a907067dca52
yupengR <2625394848@qq.com>
2020-08-26 22:48:08 +0800

    08.26

---
+++ WAV/data/message.js
@@ -0,0 +1,85 @@
+// 将消息数据从业务中分离,消息模块
+var messageList = [{
+  avatar: "/images/avatar/avatar1.jpg",
+  content: " 奉献带给我的满足感远大于获取，当看到每个孩子脸上露出灿烂的笑容时，一切努力都值得了。。。",
+  readingNum: 23,
+  commentNum: 0,
+  author: "summer",
+  dateTime: "24小时前",
+  detail: "奉献带给我的满足感远大于获取，当看到每个孩子脸上露出灿烂的笑容时，一切努力都值得了，今天参与了阳光家园的园内教学活动，整个过程非常顺利，我给孩子们辅导作业，还在最后和他们一起做游戏，度过了一段美好的时光。",
+  messageId: 1,
+  upStatus: false,
+  upNum: 11,
+  comments: []
+},
+{
+  avatar: "/images/avatar/avatar2.jpg",
+  content: "我在首页看到了武大资环青协的井盖涂鸦志愿活动，报名参加后绘制了建国七十周年主题井盖....",
+  readingNum: 12,
+  commentNum: 0,
+  author: "spring",
+  dateTime: "一天前",
+  detail: "我在首页看到了武大资环青协的井盖涂鸦志愿活动，报名参加后绘制了建国七十周年主题井盖，开心开心红红火火恍恍惚惚哈哈哈哈哈",
+  messageId: 2,
+  upStatus: false,
+  upNum: 11,
+  comments: []
+},
+{
+  avatar: "/images/avatar/avatar3.jpg",
+  content: "每一个孩子都是上天赐予人间的礼物，我参加了助残类的家教服务，这些孩子的乐观感染了我...",
+  readingNum: 23,
+  commentNum: 0,
+  author: "summer",
+  dateTime: "24小时前",
+  detail: " 每一个孩子都是上天赐予人间的礼物，我参加了助残类的家教服务，这些孩子的乐观感染了我，奉献带给我的满足感远大于获取，当看到每个孩子脸上露出灿烂的笑容时，一切努力都值得了，还在最后和他们一起做游戏，度过了一段美好的时光。",
+  messageId: 3,
+  upStatus: false,
+  upNum: 11,
+  comments: []
+},
+{
+  avatar: "/images/avatar/avatar1.jpg",
+  content: " 奉献带给我的满足感远大于获取，当看到每个孩子脸上露出灿烂的笑容时，一切努力都值得了。。。",
+  readingNum: 23,
+  commentNum: 0,
+  author: "summer",
+  dateTime: "24小时前",
+  detail: "奉献带给我的满足感远大于获取，当看到每个孩子脸上露出灿烂的笑容时，一切努力都值得了，今天参与了阳光家园的园内教学活动，整个过程非常顺利，我给孩子们辅导作业，还在最后和他们一起做游戏，度过了一段美好的时光。",
+  messageId: 4,
+  upStatus: false,
+  upNum: 11,
+  comments: []
+},
+{
+  avatar: "/images/avatar/avatar1.jpg",
+  content: " 奉献带给我的满足感远大于获取，当看到每个孩子脸上露出灿烂的笑容时，一切努力都值得了。。。",
+  readingNum: 23,
+  commentNum: 0,
+  author: "summer",
+  dateTime: "24小时前",
+  detail: "奉献带给我的满足感远大于获取，当看到每个孩子脸上露出灿烂的笑容时，一切努力都值得了，今天参与了阳光家园的园内教学活动，整个过程非常顺利，我给孩子们辅导作业，还在最后和他们一起做游戏，度过了一段美好的时光。",
+  messageId: 5,
+  upStatus: false,
+  upNum: 11,
+  comments: []
+},
+{
+  avatar: "/images/avatar/avatar1.jpg",
+  content: " 奉献带给我的满足感远大于获取，当看到每个孩子脸上露出灿烂的笑容时，一切努力都值得了。。。",
+  readingNum: 23,
+  commentNum: 0,
+  author: "summer",
+  dateTime: "24小时前",
+  detail: "奉献带给我的满足感远大于获取，当看到每个孩子脸上露出灿烂的笑容时，一切努力都值得了，今天参与了阳光家园的园内教学活动，整个过程非常顺利，我给孩子们辅导作业，还在最后和他们一起做游戏，度过了一段美好的时光。",
+  messageId: 6,
+  upStatus: false,
+  upNum: 11,
+  comments: []
+}
+
+]
+// 暴露接口
+module.exports = {
+  messageList : messageList
+}
---
+++ WAV/pages/welcome/welcome.js
@@ -0,0 +1,89 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  onTapJump: function (event) {
+    wx.switchTab({
+      url: "../message/message",
+      success: function () {
+        console.log("jump success")
+      },
+      fail: function () {
+        console.log("jump failed")
+      },
+      complete: function () {
+        console.log("jump complete")
+      }
+    });
+  },
+  // 轮播图点击跳转活动详情
+  onSwiperTap: function(event){
+    // 获得组件自定义属性
+    var messageId = event.target.dataset.messageId;
+    wx:wx.navigateTo({
+      url: 'welcome-detail/welcome-detail?id='+ messageId,
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+    },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
